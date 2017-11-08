7e650818f5d844a3dfd17444bd5277fd628cf72c
zbhphoenix <zhaobinghui1983@163.com>
2017-11-08 18:44:13 +0800

    加入application

--- app/src/main/java/com/coolweather/CoolApplication.java
+++ app/src/main/java/com/coolweather/CoolApplication.java
@@ -1,12 +1,12 @@
 package com.coolweather;
 
-import android.app.Application;
+import org.litepal.LitePalApplication;
 
 /**
  * Created by zbh on 2017/11/6.
  * Version: 1.0
  */
 
-public class CoolApplication extends Application {
+public class CoolApplication extends LitePalApplication {
 
 }
